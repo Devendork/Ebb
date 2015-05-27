1c7f46041c6727c65a28cf2254e514c39d8a79ac
Noura <culurienneldoreth@gmail.com>
2015-05-26 20:32:52 -0700

    changed the default server behavior to give the latest Mean Tide Level from Alameda

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ var http = require('http');
 
 var server = http.createServer(function(req, res) {
 
-    request("http://tidesandcurrents.noaa.gov/api/datagetter?begin_date=20130808%2015:00&end_date=20130808%2015:06&station=8454000&product=water_temperature&units=english&time_zone=gmt&application=ports_screen&format=json", function(error, response, body) {
+    request("http://tidesandcurrents.noaa.gov/api/datagetter?date=latest&station=9414750&product=one_minute_water_level&datum=MTL&units=english&time_zone=gmt&application=ports_screen&format=json", function(error, response, body) {
         res.writeHead(200, {
             "Content-Type": "text/plain",
             "Access-Control-Allow-Origin": "*",
